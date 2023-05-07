179958820e63f8cd0392d9a35a29fda80a782e8b
wu ming gang <132748374+rxyxxv@users.noreply.github.com>
2023-05-07 21:19:29 +0800

    Update myclient.cpp

--- myclient.cpp
+++ myclient.cpp
@@ -1,23 +1,16 @@
 
 #include <iostream>
 #include "Socket.hpp"
-
 using namespace std;
-
 int main(void)
 {
     try
     {
-        Socket::UDP sock;
-        
-        sock.bind(300);
-        
+        Socket::UDP sock; 
+        sock.bind(300);    
         sock.send("127.0.0.1", 2000, "request");
-        
         Socket::Datagram received = sock.receive();
-        
         cout << received.data << " " << endl;
-        
         sock.close();
     }
     catch (Socket::Exception &e)
